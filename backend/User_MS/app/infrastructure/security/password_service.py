9ff39cddd621e2102c64ae791f0b4aacc7b3ff62
Luis Mauricio Mosquera Cifuentes <luis.mosquera336@comunidadunir.net>
2026-06-24 16:51:42 -0500

    Ajuste backend

--- backend/User_MS/app/infrastructure/security/password_service.py
+++ backend/User_MS/app/infrastructure/security/password_service.py
@@ -12,5 +12,5 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     try:
         return pwd_context.verify(plain_password, hashed_password)
-    except UnknownHashError:
+    except (UnknownHashError, ValueError):
         return False
